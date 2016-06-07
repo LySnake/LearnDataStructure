c8238b380db3293da689200d36452120c02b9985
LySnake <litaobo619@163.com>
2016-06-07 23:31:53 +0800

    Update Qsort.c

--- sort/Qsort.c
+++ sort/Qsort.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-
 #define Compare(i, j) ((i) > (j) ? 1: 0)
-
 typedef int ElementType;
 
 void swap(ElementType *a , ElementType *b)
@@ -11,7 +9,12 @@ void swap(ElementType *a , ElementType *b)
     *a = *b;
     *b = temp;
 }
-
+/*******************************************************
+ * 函数功能：在Arr[Left],Arr[(Left + Right)/2],Arr[Right]中，取中间值作为基数
+ * Data:排序的数组
+ * Left:被找基数的子Arr的最左Index
+ * Right:被找基数的子Arr的最右Index
+ *****************************************************/
 ElementType Median3(ElementType Data[], int Left, int Right)
 {
     int Center = (Left + Right) / 2;
@@ -31,7 +34,12 @@ ElementType Median3(ElementType Data[], int Left, int Right)
 
     return Data[ Right - 1];
 }
-
+/*********************************************************************
+ * 函数功能:快排，对于一个数组，选用一个基数，比基数大的，放于相对右，比基数小的，放于相对左边，中间放基数。左右二边再分边Qsort。可以设置当arr的元素个数不足100时使用冒泡算法，n相对小时，复杂排序反而优势不足。
+ *Data:被排数组
+ * Left:最左元素
+ * Right:最右元素
+ ******************************************************************/
 
 void Qsort(ElementType Data[], int Left, int Right)
 {
